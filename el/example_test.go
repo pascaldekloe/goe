27ca348cd12ec64421492bb1183d09bb52c6294a
Pascal S. de Kloe <pascal@quies.net>
2023-03-26 19:18:30 +0200

    broken example drop

--- el/example_test.go
+++ el/example_test.go
@@ -1,7 +1,6 @@
 package el_test
 
 import (
-	"crypto/tls"
 	"fmt"
 	"image/gif"
 	"strings"
@@ -34,41 +33,6 @@ func ExampleAssign_typeFlexibility() {
 
 	fmt.Printf("%+v", x)
 	// Output: &{B:42 I:42 F:42}
-}
-
-func ExampleInt() {
-	certPEM := []byte(`-----BEGIN CERTIFICATE-----
-MIICADCCAaegAwIBAgIJAN6R4f75XSwGMAkGByqGSM49BAEwODELMAkGA1UEBhMC
-TkwxFTATBgNVBAgTDFp1aWQgSG9sbGFuZDESMBAGA1UEChMJUXVpZXMgTmV0MB4X
-DTE1MDcxOTE2Mjc1M1oXDTE2MDcxODE2Mjc1M1owODELMAkGA1UEBhMCTkwxFTAT
-BgNVBAgTDFp1aWQgSG9sbGFuZDESMBAGA1UEChMJUXVpZXMgTmV0MFkwEwYHKoZI
-zj0CAQYIKoZIzj0DAQcDQgAEMpDspvp882GPuJ2/Hk/Ve3z77/QGHDjy3RcAX5gs
-0h/6IGX16aR5LQmdIOfhTbJnrcuwspzDAlLHsRRYnFFUiKOBmjCBlzAdBgNVHQ4E
-FgQUZsY7o2QXCbSUztcvob5IN3CnKz0waAYDVR0jBGEwX4AUZsY7o2QXCbSUztcv
-ob5IN3CnKz2hPKQ6MDgxCzAJBgNVBAYTAk5MMRUwEwYDVQQIEwxadWlkIEhvbGxh
-bmQxEjAQBgNVBAoTCVF1aWVzIE5ldIIJAN6R4f75XSwGMAwGA1UdEwQFMAMBAf8w
-CQYHKoZIzj0EAQNIADBFAiBw/N+/GobvuxCbzbd84rca4lghmQjw+MQChNO+or7m
-+wIhAMONJleFsloA0+PSanth2gRMl3yMlXtRjzmdjcBhoBE6
------END CERTIFICATE-----`)
-
-	keyPEM := []byte(`-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIL1IxRlbbIhUiIhSfKkksuCaVf2Bl4BQQ9whrOiVWt8voAoGCCqGSM49
-AwEHoUQDQgAEMpDspvp882GPuJ2/Hk/Ve3z77/QGHDjy3RcAX5gs0h/6IGX16aR5
-LQmdIOfhTbJnrcuwspzDAlLHsRRYnFFUiA==
------END EC PRIVATE KEY-----`)
-
-	cert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-
-	bs, ok := el.Int("/PrivateKey/PublicKey/Curve/CurveParams/BitSize", cert)
-	if !ok {
-		panic("lookup fail")
-	}
-
-	fmt.Printf("size: %d", bs)
-	// Output: size: 256
 }
 
 func ExampleUints() {
